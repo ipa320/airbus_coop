88b88f177cc3984c1e2fdce60b1713d5eb39e45a
ipa-nhg <nhg@ipa.fhg.de>
2017-07-26 16:12:32 +0200

    Review CMakelists and package files

--- plugins/plugin_rqt/setup.py
+++ plugins/plugin_rqt/setup.py
@@ -17,7 +17,7 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-	packages=['plugin_node_manager','plugin_node_manager.v2'],
+	packages=['plugin_rqt'],
 	package_dir={'': 'src'},
 )
 
