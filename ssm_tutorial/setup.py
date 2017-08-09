bf7c3e6ea9a3e0acef41f065ee861f2b669d39c2
ipa-led <ludovic.delval@ipa.fraunhofer.de>
2017-08-09 11:43:43 +0200

    fixed setup.py for travis

--- ssm_tutorial/setup.py
+++ ssm_tutorial/setup.py
@@ -17,7 +17,9 @@ from distutils.core import setup
 from catkin_pkg.python_setup import generate_distutils_setup
 
 d = generate_distutils_setup(
-	packages=['ssm_tutorial'],
+	packages=['ssm_tutorial',
+			'ssm_test_skills',
+			'ssm_skill_examples'],
 	package_dir={'': 'src'},
 )
 
