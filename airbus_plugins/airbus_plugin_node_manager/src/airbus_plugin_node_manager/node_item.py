6cbf2d455c914f2b45be6e653bbaa20655c493d7
Ludovic <led@ipa.fhg.com>
2017-10-12 10:37:56 +0200

    Plugin node manager ported to PyQt5

--- airbus_plugins/airbus_plugin_node_manager/src/airbus_plugin_node_manager/node_item.py
+++ airbus_plugins/airbus_plugin_node_manager/src/airbus_plugin_node_manager/node_item.py
@@ -27,6 +27,7 @@ from roslib.packages import get_pkg_dir
 
 from python_qt_binding.QtGui import *
 from python_qt_binding.QtCore import *
+from python_qt_binding.QtWidgets import *
 
 def widget_creator(obj_ui):
     
